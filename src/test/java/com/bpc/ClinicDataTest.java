fac9477fdc5180f064206d2c2fe8834d2c4aba31
Leonard-Ikeh-Nnabugo <li24aad@herts.ac.uk>
2025-04-16 12:11:07 +0100

    Created test for bookAppointment functionality

--- src/test/java/com/bpc/ClinicDataTest.java
+++ src/test/java/com/bpc/ClinicDataTest.java
@@ -11,27 +11,56 @@ class ClinicDataTest {
         int totalNumOfPatients = instance.getTotalEverNumOfPatients();
         instance.addPatient("Abedi Pele","Some address","+44904438373");
 
-        int expected = instance.getTotalEverNumOfPatients();
-        assertEquals(expected,totalNumOfPatients+1);
+        int  expected = totalNumOfPatients+1;
+        int result = instance.getTotalEverNumOfPatients();
+        assertEquals(expected,result); //expect total number of patients to increase by 1
     }
 
     @Test
     void getTotalEverNumOfPatients() {
         System.out.println("addPatient");
         ClinicData instance = new ClinicData();
-        int expected = instance.getTotalEverNumOfPatients();
-        int actual = 10; // 10 patients get loaded via ClinicData constructor initially
-
-        assertEquals(expected,actual);
+        int result = instance.getTotalEverNumOfPatients();
+        int expected = 10; // 10 patients get loaded via ClinicData constructor initially
+        assertEquals(expected,result); //expect the initial number of patients to be 10
 
         //test when another patient is added
         instance.addPatient("Abedi Pele","Some address","+44904438373");
-        expected = instance.getTotalEverNumOfPatients();
-        actual = 11;
-        assertEquals(expected,actual);
+        expected++; //Number of patients should increase by 1
+        result = instance.getTotalEverNumOfPatients();
+        assertEquals(expected,result);
     }
 
     @Test
     void testBookAppointment() {
+        System.out.println("bookAppointment");
+        ClinicData instance = new ClinicData();
+        int totalNumOfAppointments = instance.getTotalNumOfAppointments();
+
+        instance.bookAppointment("2025-02-03 10","patient-1","physio-1");
+
+        int  expected = totalNumOfAppointments+1;
+        int result = instance.getTotalNumOfAppointments();
+        assertEquals(expected,result); //passes of appointment got booked
+
+        //Test when an invalid patient id is passed
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,()-> instance.bookAppointment("2025-02-03 10","pat-1","physio-1")); //pat-1 is not an available patient id
+        assertEquals("Patient is invalid", ex.getMessage());
+
+        //Test when an invalid physiotherapist id is passed
+        IllegalArgumentException ex2 = assertThrows(IllegalArgumentException.class,()-> instance.bookAppointment("2025-02-03 10","patient-1","physio-100")); //physio-100 is not an available physio id
+        assertEquals("Physiotherapist is invalid", ex2.getMessage());
+
+        //test for when a schedule is not available for a physiotherapist
+        IllegalArgumentException ex3 = assertThrows(IllegalArgumentException.class,()-> instance.bookAppointment("2025-02-03 12","patient-1","physio-1")); //this datetime is not part of the physios schedule
+        assertEquals("Schedule not available for physiotherapist", ex3.getMessage());
+
+        //test when the schedule has already been booked or attended
+        IllegalArgumentException ex4 = assertThrows(IllegalArgumentException.class,()-> instance.bookAppointment("2025-02-03 10","patient-2","physio-1")); //this was already booked by patient-1 in our first edge-case
+        assertEquals("Appointment already booked or attended", ex4.getMessage());
+    }
+
+    @Test
+    void getTotalNumOfAppointments() {
     }
 }
