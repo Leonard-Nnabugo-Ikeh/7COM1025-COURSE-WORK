af670dec017418a7a0838ed5a9338c32efe26d40
Leonard-Ikeh-Nnabugo <li24aad@herts.ac.uk>
2025-04-26 14:22:39 +0100

    Updated formatToDisplayDate test

--- src/test/java/com/bpc/UtilsTest.java
+++ src/test/java/com/bpc/UtilsTest.java
@@ -14,13 +14,16 @@ class UtilsTest {
         String dateTime = "2025-02-03 10";
         String expected = "Monday 3rd of February 2025, 10:00-11:00";
         String result = instance.formatToDisplayDate(dateTime);
+        assertEquals(expected, result);
 
         dateTime = "2025-02-06 09";
         expected = "Thursday 6th of February 2025, 09:00-10:00";
         result = instance.formatToDisplayDate(dateTime);
+        assertEquals(expected, result);
 
         dateTime = "2025-02-20 16";
         expected = "Thursday 20th of February 2025, 16:00-17:00";
         result = instance.formatToDisplayDate(dateTime);
+        assertEquals(expected, result);
     }
 }
